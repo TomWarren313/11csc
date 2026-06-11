7ebbae9c383806715a1475b7c06fbfc6c9b58fe5
TomWarren313 <tw24202@students.sacredheart.school.nz>
2026-06-11 13:52:55 +1200

    idk

--- advancedloopsandlist2.py
+++ advancedloopsandlist2.py
@@ -1,10 +1,36 @@
+best_time = 0
+goal_time = 50
+flag = True
 held_times = []
 def get_data():
-    try:
-        time = input('Please enter a time: ')
+      try:
+        time = input('How long did you run for? ')
         time = int(time)
         if time > 0:
             held_times.append(time)
-    except ValueError:
+      except ValueError:
         print('Im sorry but what you entered is invalid! Try Again.')
-    return time
+      return time
+
+flag2 = False
+while flag2:
+    run_time = get_data()
+
+    if run_time > best_time:
+      best_time = run_time
+      print(f'A personal best! {best_time} seconds is your best time.')
+
+    if run_time >= goal_time:
+      print('You reached your goal!!!')
+      flag2 = True
+
+print('Recording ended.')
+
+if len(held_times) == 0:
+  print('Sorry, you have not entered any valid times.')
+else:
+  print('Your results are:')
+
+held_times.sort(reverse= True)
+for num in held_times:
+  print(num, 'seconds')
