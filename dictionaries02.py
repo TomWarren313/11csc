757aa546c7e130cb762a3cd9ade4bbd69d0833fe
TomWarren313 <tw24202@students.sacredheart.school.nz>
2026-06-24 10:22:00 +1200

    continued work on dictionaries

--- dictionaries02.py
+++ dictionaries02.py
@@ -1,9 +1,28 @@
-favourites = {}
+#function to get data
+def getData():
+    car = input('Please enter a car brand: ').title()
+    return car
 
-brand = input('brand: ')
-while brand:
-  favourites[brand]
-  brand = input('brand: ')
+#function that deals with processing and sorting the dictionary
+def processing():
+  car_brands = {}
+  flag = True
+  while flag:
+    key = getData()
+    if key == '':
+        flag = False
+        return car_brands
+#If something is already in dictionary, value gets + 1, if it isn't in the dictionary it gets added with a value of one
+    elif key in car_brands:
+        car_brands[key] += 1
+    else:
+        car_brands[key] = 1
+#function that is used to get our output and print it
+def output():
+  my_car_brands = processing()
+  for key,value in my_car_brands.items():
+#I prefer to use commas instead of f-strings as it looks more precise
+    print(key, 'has been entered', value, 'time(s)')
 
-for brand in favourites:
-  print(brand, favourites[brand])
+#main routine
+output()
