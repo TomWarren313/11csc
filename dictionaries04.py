6c62359e4672690d6d97970b393734b70663ebef
TomWarren313 <tw24202@students.sacredheart.school.nz>
2026-06-26 10:41:04 +1200

    Peak 😜😘😜😘

--- dictionaries04.py
+++ dictionaries04.py
@@ -2,7 +2,7 @@ def getData():
     flag = True
     while flag:
         try:
-            student = input('Enter the student\'s name or leave blank to terminate entry: ')
+            student = input("Enter the student's name or leave blank to terminate entry: ")
             if student == '':
                 return '', -999  
             
@@ -13,7 +13,7 @@ def getData():
                 print('Error: Attendance must be between 0 and 100.')
                 
         except ValueError:
-            print('I\'m sorry but this number creates an error, please aim to only have integers inputted!')
+            print("I'm sorry but this number creates an error, please aim to only have integers inputted!")
 
 def processing():
     all_students = {}
