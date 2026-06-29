5af476f6f52a8fbbdb7764e451d55db67abfdd9c
TomWarren313 <tw24202@students.sacredheart.school.nz>
2026-06-29 15:06:53 +1200

    made output into a function

--- dictionaries04.py
+++ dictionaries04.py
@@ -6,9 +6,9 @@ def getData():
     flag = True
     while flag:
         try:
-            student = input("Enter the student's name or leave blank to terminate entry: ")
+            student = input("Enter the student's name or leave blank to terminate entry: ").title()
             if student == '':
-                return '', -999  
+                return '', -999
             
             attendance = int(input('Enter the student attendance: '))
             if 0 <= attendance <= 100:
@@ -20,17 +20,26 @@ def getData():
             print("I'm sorry but this number creates an error, please aim to only have integers inputted!")
 
 def processing():
+    total = 0
     all_students = {}
     flag2 = True
     while flag2:
         my_student, the_attendance = getData()
         if my_student == '': 
+            average = total / len(all_students)
             flag2 = False
         else:
+           total = total + the_attendance
            all_students[my_student] = the_attendance
-    return all_students
+    return all_students, average
 
-# Main routine
-output = processing()
-for key, value in output.items():
-    print(key, 'attendance is', f"{value}%")
+def output():
+  print()
+  print('Results entered. Now printing attendance and overall average')
+  output, my_average = processing()
+  for key, value in output.items():
+      print(f"{key}'s", 'attendance is', f"{value}%")
+  print(f'Average attendance throughout all students: {my_average}%')
+
+#main routine
+output()
